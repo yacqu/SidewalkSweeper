b52500ad84ff672a4b23ed676fa8ded3a6bb5861
invent7 <135671197+invent7@users.noreply.github.com>
2023-06-16 10:41:33 -0700

    Add files via upload

---
+++ src/Capstone_Stepper_Code.ino
@@ -0,0 +1,37 @@
+//This code will make the stepper motor chaft swing back and forth
+
+// Include the AccelStepper Library
+#include <AccelStepper.h>
+
+// Define pin connections
+const int dirPin = 2;
+const int stepPin = 3;
+
+// Define motor interface type
+#define motorInterfaceType 1
+
+// Creates an instance
+AccelStepper myStepper(motorInterfaceType, stepPin, dirPin);
+
+void setup() {
+  // set the maximum speed, acceleration factor,
+  // initial speed and the target position
+  pinMode(10, INPUT_PULLUP);
+  myStepper.setMaxSpeed(1000);
+  myStepper.setAcceleration(50);
+  myStepper.setSpeed(600);
+  myStepper.moveTo(400);
+}
+
+void loop() {
+  // Change direction once the motor reaches target position
+  if (myStepper.distanceToGo() == 0) 
+    myStepper.moveTo(-myStepper.currentPosition());
+  // Move the motor one step
+  myStepper.run();
+  
+  while (digitalRead(10) == LOW) {
+  
+  // Do nothing
+  }
+}
